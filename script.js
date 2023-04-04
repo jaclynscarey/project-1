32eaec8196d451e67dbc1c3bc4bc47bb4408cc29
jaclynscarey <jsoyama@gmail.com>
2023-04-04 14:39:42 -0400

    wip: created functions for rendering new questions

--- script.js
+++ script.js
@@ -218,43 +218,104 @@ let playerName;
 let numOfQuestions;
 let currentQuestions;
 let score;
+let randomQuestions;
+let position;
+let newQuestionEl;
+let newChoice1El;
+let newChoice2El;
+let newChoice3El;
+let newChoice4El;
+let nextBtn;
+
 
 
 // cached elements
 const nameInputEl = document.getElementById('name-input');
 const numInputEl = document.getElementById('num-input');
-const playBtn = document.querySelector('button');
+const playBtn = document.getElementById('play-button');
+const mainContainerEl = document.getElementById('main-container');
+
 
 // event listeners
-playBtn.addEventListener('click', initialize);
+playBtn.addEventListener('click', play);
 
 // functions
-initialize ();
+initialize();
 
+// resets all to initial values
 function initialize() {
-    playerName = nameInputEl.value;
-    numOfQuestions = numInputEl.value;
+    playerName = '';
+    numOfQuestions = 0;
     currentQuestions = [];
     score = 0;
+    randomQuestions = [];
+    position = 0;
+}
+// takes player input and starts game
+function play() {
+    playerName = nameInputEl.value;
+    numOfQuestions = numInputEl.value;
+    nameInputEl.value = '';
+    numInputEl.value = '';
+    document.getElementById('home-div').style.visibility = "hidden";
+    document.getElementById('home-div').style.height = '0px';
+    randomQuestions = randomize(questionsBank);
+    currentQuestions = questionsPicker(randomQuestions);
+    position++;
+    createNewLayout();
+}
 
-   
+function nextQuestion() {
+    position++;
+    createNewLayout();
+
+}
+
+function createNewLayout() {
+    newQuestionEl = document.createElement('h2');
+    newQuestionEl.classList = `question-${position}`;
+    newQuestionEl.innerText = currentQuestions[position-1].question;
+    mainContainerEl.appendChild(newQuestionEl);
+
+    newChoice1El = document.createElement('h3');
+    newChoice1El.innerText = currentQuestions[position-1].choices.a;
+    mainContainerEl.appendChild(newChoice1El);
+
+    newChoice2El = document.createElement('h3');
+    newChoice2El.innerText = currentQuestions[position-1].choices.b;
+    mainContainerEl.appendChild(newChoice2El);
+
+    newChoice3El = document.createElement('h3');
+    newChoice3El.innerText = currentQuestions[position-1].choices.c;
+    mainContainerEl.appendChild(newChoice3El);
+
+    newChoice4El = document.createElement('h3');
+    newChoice4El.innerText = currentQuestions[position-1].choices.d;
+    mainContainerEl.appendChild(newChoice4El);
+
+    nextBtn = document.createElement('button');
+    nextBtn.innerText = 'Next';
+    mainContainerEl.appendChild(nextBtn);
+    nextBtn.addEventListener('click', nextQuestion);
 }
 
 // randomize() shuffles all questions in questionsBank array
-function randomize(questions) {
+function randomize(randomQuestions) {
     let index = questionsBank.length;
     let randomIndex;
     while (index !== 0) {
         randomIndex = Math.floor(Math.random() * index);
-        console.log(randomIndex);
         index--;
-        [questions[index], questions[randomIndex]] = [questions[randomIndex], questions[index]];
+        [randomQuestions[index], randomQuestions[randomIndex]] = [randomQuestions[randomIndex], randomQuestions[index]];
     }
-    return questions;
+    return randomQuestions;
 }
 
 // questionsPicker() chooses user's requested # of questions from randomize()
 function questionsPicker() {
-
+    for (let i = 0; i < numOfQuestions; i++) {
+        currentQuestions.push(randomQuestions[i]);
+    }
+    return currentQuestions;
 }
 
